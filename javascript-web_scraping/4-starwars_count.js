5339f972c2c90cc2c9b17bde3f3833d2f4d5d339
ER1-53 <cremey.erwan@gmail.com>
2024-01-22 18:17:03 +0100

    update task 4 change all code task 6 change variable

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,29 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes('/18/')) {
+          count += 1;
+          break;
+        }
+      }
+    }
+
+    console.log(count);
+  }
+});
+
+/*const request = require('request');
 
 const apiUrl = process.argv[2];
 const characterId = 18;
@@ -13,4 +36,4 @@ request.get(apiUrl, (error, response, body) => {
   } else {
     console.error(error || `Erreur de requête. Statut : ${response ? response.statusCode : 'Inconnu'}`);
   }
-});
+});*/
--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,31 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, (error, reponse, body) => {
-  if (reponse && reponse.statusCode === 200) {
-    const list = JSON.parse(body);
-
-    const completedByUser = {};
-
-    list.forEach((userInfo) => {
-      const { userId } = userInfo;
-
-      if (!completedByUser[userId]) {
-        completedByUser[userId] = 0;
-      }
-      if (userInfo.completed) {
-        completedByUser[userId]++;
-      }
-    });
-    // pour le transposer en Objet
-    /* Object.keys(completedByUser).forEach(userId => {
-			console.log(`${userId}: ${completedByUser[userId]}`);
-		}); */
-    console.log(completedByUser);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   } else {
-    console.error(error);
+    const todos = JSON.parse(body);
+    const tasksCompleted = {};
+
+    for (const task of todos) {
+      if (task.completed) {
+        if (tasksCompleted[task.userId]) {
+          tasksCompleted[task.userId] += 1;
+        } else {
+          tasksCompleted[task.userId] = 1;
+        }
+      }
+    }
+
+    console.log(tasksCompleted);
   }
 });
