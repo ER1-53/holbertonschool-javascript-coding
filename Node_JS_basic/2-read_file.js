f7edda447075ae4e9a38bca1ae865f3999476ca1
ER1-53 <cremey.erwan@gmail.com>
2024-01-31 17:31:38 +0100

    change repo name

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let data;
+  try {
+    data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = data.split('\n');
+  const students = lines.filter(line => line).map(line => line.split(','));
+
+  const numberOfStudents = students.length;
+  console.log(`Number of students: ${numberOfStudents - 1}`);
+
+  const fields = {};
+  for (const student of students) {
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(student[0]);
+  }
+
+  for (const field in fields) {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
