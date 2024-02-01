960a57f9aaa0d3f21823af1614bc56aa86318b66
ER1-53 <cremey.erwan@gmail.com>
2024-02-01 13:56:34 +0100

    update nice tack1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,21 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+function welcome() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('data', (input) => {
+    const name = input.toString();
+    process.stdout.write(`Your name is: ${name}`);
+
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
+}
+
+module.exports = welcome;
+
+welcome();
+
+/*process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 new Promise((resolve) => {
@@ -9,5 +26,5 @@ new Promise((resolve) => {
   process.stdout.write(`Your name is: ${name}`);
   process.stdout.write('This important software is now closing\n');
   process.exit();
-});
+});*/
 
