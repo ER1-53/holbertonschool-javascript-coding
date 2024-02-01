3ab86e19a6723e8c60e9da150d496b246898283d
ER1-53 <cremey.erwan@gmail.com>
2024-02-01 14:23:28 +0100

    update task 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -10,20 +10,17 @@ async function countStudents(path) {
 
   const lines = data.split('\n');
   const students = lines.filter(line => line).map(line => line.split(','));
-  console.log(students);
   const numberOfStudents = students.length;
   console.log(`Number of students: ${numberOfStudents}`);
 
   const fields = {};
   for (const student of students) {
     const field = student[3];
-    console.log(fields[field]);
     if (!fields[field]) {
       fields[field] = [];
     }
     fields[field].push(student[0]);
   }
-
   for (const field in fields) {
     console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
   }
