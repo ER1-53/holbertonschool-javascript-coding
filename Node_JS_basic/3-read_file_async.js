77a3e370897cba1d6f4b0a33888d66b488cc132b
ER1-53 <cremey.erwan@gmail.com>
2024-02-02 09:32:03 +0100

    update task 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,39 +2,39 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.promises.readFile(path, 'utf8')
-      .then((data) => {
-        const lines = data.split('\n');
-        const count = lines.length - 1;
-        let countCS = 0;
-        let countSWE = 0;
-        const CSstudient = [];
-        const SWEstudient = [];
-        const CS = 'CS';
-        const SWE = 'SWE';
-        for (let i = 1; i < lines.length; i++) {
-          const colonnes = lines[i].split(',');
-          if (colonnes[3] === 'CS') {
-            countCS++;
-            CSstudient.push(` ${colonnes[0]}`);
-          } else if (colonnes[3] === 'SWE') {
-            countSWE++;
-            SWEstudient.push(` ${colonnes[0]}`);
-          }
+    fs.readFile(path, 'utf8', (error, data) => {
+      const lines = data.split('\n');
+      const count = lines.length - 1;
+      let countCS = 0;
+      let countSWE = 0;
+      const CSstudient = [];
+      const SWEstudient = [];
+      const CS = 'CS';
+      const SWE = 'SWE';
+      for (let i = 1; i < lines.length; i++) {
+        const colonnes = lines[i].split(',');
+        if (colonnes[3] === 'CS') {
+          countCS++;
+          CSstudient.push(` ${colonnes[0]}`);
+        } else if (colonnes[3] === 'SWE') {
+          countSWE++;
+          SWEstudient.push(` ${colonnes[0]}`);
         }
-        const result = {
-          total: `Number of students: ${count}`,
-          cs: `Number of students in ${CS}: ${countCS}. List:${CSstudient}`,
-          swe: `Number of students in ${SWE}: ${countSWE}. List:${SWEstudient}`,
-        };
-        console.log(result.total);
-        console.log(result.cs);
-        console.log(result.swe);
-        resolve(result);
-      })
-      .catch((error) => {
+      }
+      const result = {
+        total: `Number of students: ${count}`,
+        cs: `Number of students in ${CS}: ${countCS}. List:${CSstudient}`,
+        swe: `Number of students in ${SWE}: ${countSWE}. List:${SWEstudient}`,
+      };
+      console.log(result.total);
+      console.log(result.cs);
+      console.log(result.swe);
+      resolve(result);
+
+      if (error) {
         reject(new Error('Cannot load the database'));
-      });
+      }
+    });
   });
 }
 
