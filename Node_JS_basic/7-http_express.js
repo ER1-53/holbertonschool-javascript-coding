53370064e7db17c54d6a86b7cfc19454ffb880fb
ER1-53 <cremey.erwan@gmail.com>
2024-02-02 14:50:39 +0100

    eslint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
+
 const path = process.argv[2];
 
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
 app.get('/students', (req, res) => {
-    const outp = 'This is the list of our students';
-    countStudents(path)
+  const outp = 'This is the list of our students';
+  countStudents(path)
     .then((result) => {
-        const rep = `${outp}\n${result.total}\n${result.CS}\n${result.SWE}`;
-        res.type('text/plain').send(rep);
+      const rep = `${outp}\n${result.total}\n${result.CS}\n${result.SWE}`;
+      res.type('text/plain').send(rep);
     })
     .catch((err) => {
-        const rep = `${outp}\n${err.message}`;
-        res.type('text/plain').send(rep);
-    })
+      const rep = `${outp}\n${err.message}`;
+      res.type('text/plain').send(rep);
+    });
 });
 
 app.listen(port);
