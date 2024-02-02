ad116d4652e0bcb8b7eacbad9642adb41925c1c1
ER1-53 <cremey.erwan@gmail.com>
2024-02-02 09:23:05 +0100

    task 3 real update

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,29 +1,41 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-async function countStudents(path) {
-  let data;
-  try {
-    data = await fs.readFile(path, { encoding: 'utf8' });
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-
-  const lines = data.split('\n');
-  const students = lines.filter((line) => line).map((line) => line.split(','));
-  const numberOfStudents = students.length;
-  console.log(`Number of students: ${numberOfStudents}`);
-
-  const fields = {};
-  for (const student of students) {
-    const field = student[3];
-    if (!fields[field]) {
-      fields[field] = [];
-    }
-    fields[field].push(student[0]);
-  }
-  for (const field in fields) {
-    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-  }
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.promises.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const count = lines.length - 1;
+        let countCS = 0;
+        let countSWE = 0;
+        const CSstudient = [];
+        const SWEstudient = [];
+        const CS = 'CS';
+        const SWE = 'SWE';
+        for (let i = 1; i < lines.length; i++) {
+          const colonnes = lines[i].split(',');
+          if (colonnes[3] === 'CS') {
+            countCS++;
+            CSstudient.push(` ${colonnes[0]}`);
+          } else if (colonnes[3] === 'SWE') {
+            countSWE++;
+            SWEstudient.push(` ${colonnes[0]}`);
+          }
+        }
+        const result = {
+          total: `Number of students: ${count}`,
+          cs: `Number of students in ${CS}: ${countCS}. List:${CSstudient}`,
+          swe: `Number of students in ${SWE}: ${countSWE}. List:${SWEstudient}`,
+        };
+        console.log(result.total);
+        console.log(result.cs);
+        console.log(result.swe);
+        resolve(result);
+      })
+      .catch((error) => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
 module.exports = countStudents;
---
+++ Node_JS_basic/3-read_file_asyncv1.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  let data;
+  try {
+    data = await fs.readFile(path, { encoding: 'utf8' });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = data.split('\n');
+  const students = lines.filter((line) => line).map((line) => line.split(','));
+  const numberOfStudents = students.length;
+  console.log(`Number of students: ${numberOfStudents}`);
+
+  const fields = {};
+  for (const student of students) {
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(student[0]);
+  }
+  for (const field in fields) {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
