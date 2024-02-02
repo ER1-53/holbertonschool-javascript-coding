85b8ee91e3c8a30d9a80232c4f79f313bee3aa47
ER1-53 <cremey.erwan@gmail.com>
2024-02-02 09:04:54 +0100

    update eslint task 3 & 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -5,9 +5,8 @@ const port = 1245;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end("Hello Holberton School!");
+  res.end('Hello Holberton School!');
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host);
-
