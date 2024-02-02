9533493e7111c68e467042d592001b6b8795ecb9
ER1-53 <cremey.erwan@gmail.com>
2024-02-02 14:41:48 +0100

    task7 created

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+const path = process.argv[2];
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!')
+})
+
+app.get('/students', (req, res) => {
+    const outp = 'This is the list of our students';
+    countStudents(path)
+    .then((result) => {
+        const rep = `${outp}\n${result.total}\n${result.CS}\n${result.SWE}`;
+        res.send(rep);
+    })
+    .catch((err) => {
+        const rep = `${outp}\n${err.message}`;
+        res.send(rep);
+    })
+});
+
+app.listen(port);
+
+module.exports = app;
