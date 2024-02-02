648e670ac44e79fe1db910d5203f9e8f9a992d18
ER1-53 <cremey.erwan@gmail.com>
2024-02-02 09:00:39 +0100

    try task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
 
-new Promise((resolve) => {
-  process.stdin.on('data', (input) => {
-    resolve(input.toString());
-  });
-}).then((name) => {
-  process.stdout.write(`Your name is: ${name}`);
-  process.exit()
-});
-
-process.on('exit', (code) => {
+process.stdin.on('data', (input) => {
+  const reponse = input.toString().trim();
+  process.stdout.write(`Your name is: ${reponse}\n`);
   process.stdout.write('This important software is now closing\n');
-});
-
-process.on('SIGINT', function() {
   process.exit();
 });
