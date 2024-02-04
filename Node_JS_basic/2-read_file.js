eeda303cd6f76e456d7ecc2d5f1e8f000b47d84b
ER1-53 <cremey.erwan@gmail.com>
2024-02-04 11:13:57 +0100

    change output

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -13,16 +13,16 @@ function countStudents(path){
     for (const line of lines) {
       const cutLine = line.split(',');
       if (cutLine[3] === 'CS'){
-        csArray.push(' ' + cutLine[0]);
+        csArray.push(cutLine[0]);
         countCS++;
       } else if (cutLine[3] === 'SWE'){
-        sweArray.push(' ' +cutLine[0]);
+        sweArray.push(cutLine[0]);
         countSWE++;
       }
     }
     console.log(`Number of students: ${count}`);
-    console.log(`Number of students in CS: ${countCS}. List:${csArray}`)
-    console.log(`Number of students in SWE: ${countSWE}. List:${sweArray}`)
+    console.log(`Number of students in CS: ${countCS}. List:${csArray.join(', ')}`)
+    console.log(`Number of students in SWE: ${countSWE}. List:${sweArray.join(', ')}`)
   
     } catch(err) {
       throw Error('Cannot load the database')
