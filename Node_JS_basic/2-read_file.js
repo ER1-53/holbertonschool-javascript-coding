7df638d2278fd8499c2c1fca413e674b4a7e58c8
ER1-53 <cremey.erwan@gmail.com>
2024-02-04 11:14:42 +0100

    eslint correction

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,32 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
 
-function countStudents(path){
-
+function countStudents(path) {
   try {
     const doc = fs.readFileSync(path, 'utf8');
     const lines = doc.split('\n');
     let countCS = 0;
     let countSWE = 0;
     csArray = [];
-    sweArray = []
-    let count = lines.length - 1;
+    sweArray = [];
+    const count = lines.length - 1;
     for (const line of lines) {
       const cutLine = line.split(',');
-      if (cutLine[3] === 'CS'){
+      if (cutLine[3] === 'CS') {
         csArray.push(cutLine[0]);
         countCS++;
-      } else if (cutLine[3] === 'SWE'){
+      } else if (cutLine[3] === 'SWE') {
         sweArray.push(cutLine[0]);
         countSWE++;
       }
     }
     console.log(`Number of students: ${count}`);
-    console.log(`Number of students in CS: ${countCS}. List:${csArray.join(', ')}`)
-    console.log(`Number of students in SWE: ${countSWE}. List:${sweArray.join(', ')}`)
-  
-    } catch(err) {
-      throw Error('Cannot load the database')
-    }
+    console.log(`Number of students in CS: ${countCS}. List:${csArray.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSWE}. List:${sweArray.join(', ')}`);
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
